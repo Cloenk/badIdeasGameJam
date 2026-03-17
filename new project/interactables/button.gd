09c9f8efed48e1650269ce4ad2a62535507e3afc
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2026-03-17 00:25:35 -0400

    make button able to move object

--- new project/interactables/button.gd
+++ new project/interactables/button.gd
@@ -15,7 +15,13 @@ enum ButtonMode {
 	VariableBlend,
 }
 
-@export_node_path("Node3D") var nodepath: NodePath
+var has_valid_nodepath: bool = false
+@export_node_path("Node3D") var nodepath: NodePath:
+	set(value):
+		if value.get_name_count() > 0:
+			print("connected to " + value.get_concatenated_names())
+			has_valid_nodepath = true
+			nodepath = value.slice(0, value.get_name_count())
 
 @export var mode: ButtonMode = ButtonMode.TransformSerial
 
@@ -36,6 +42,128 @@ enum ButtonMode {
 @export var rel_to_previous: int = -1
 @export var rel_from_this_position: bool = true
 @export var rel_from_this_rotation: bool = false
+
+var is_running: bool = false
+var preview: bool = false
+var apply_if_valid: bool = false
+var reset_transform: bool = false
+
+var serial_index: int = 0
+var serial_last_index: int = 0
+var serial_index_preview: int = 0
+var serial_last_index_preview: int = 0
+
+var blend_index: int = 0
+var blend_last_index: int = 0
+
+var last_quat: Quaternion
+var next_quat: Quaternion
+var last_pos: Vector3
+var next_pos: Vector3
+var last_scale: Vector3
+var next_scale: Vector3
+var original_transform: Transform3D
+var current_transform: Transform3D
+
+var accumulator: float = 0
+var increment: float = 0
+var inc_count: int = 1
+var run_count: int = 0
+
+@export_group("Action")
+@export_tool_button("Preview Transform Gizmo") var preview_transform_gizmo_button = transform_button_function.bind(ButtonEditorButtons.PreviewTransformGizmo)
+@export_tool_button("Preview Transform Object") var preview_transform_object_button = transform_button_function.bind(ButtonEditorButtons.PreviewTransformObject)
+@export_tool_button("Transform Object") var transform_object_button = transform_button_function.bind(ButtonEditorButtons.TransformObject)
+enum ButtonEditorButtons {
+	PreviewTransformGizmo,
+	PreviewTransformObject,
+	TransformObject
+}
+func transform_button_function(which_button: ButtonEditorButtons):
+	print(which_button)
+	if has_valid_nodepath:
+		var was_running = is_running
+		accumulator = 0
+		run_count = 0
+		is_running = true
+		match which_button:
+			ButtonEditorButtons.PreviewTransformGizmo:
+				preview = true
+				apply_if_valid = false
+				reset_transform = false
+			ButtonEditorButtons.PreviewTransformObject:
+				preview = true
+				apply_if_valid = true
+				reset_transform = true
+			ButtonEditorButtons.TransformObject:
+				preview = false
+				apply_if_valid = true
+				reset_transform = false
+		increment = 1.0 / (blendtime * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
+		inc_count = 1 + (blendtime * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
+		if not was_running:
+			original_transform = get_node(nodepath).global_transform
+		var serial_or_random: bool = false
+		match which_button:
+			ButtonEditorButtons.PreviewTransformGizmo, ButtonEditorButtons.PreviewTransformObject:
+				if mode == ButtonMode.TransformSerial or mode == ButtonMode.VariableSerial:
+					serial_or_random = true
+					serial_index_preview += 1
+					if serial_index_preview >= len(transforms):
+						serial_index_preview = 0
+					serial_last_index_preview = serial_index_preview - 1
+					if serial_last_index_preview < 0:
+						serial_last_index_preview = len(transforms) - 1
+				elif mode == ButtonMode.TransformRandom or mode == ButtonMode.VariableRandom:
+					serial_or_random = true
+					var next_idx = -1
+					for i in range(0, 100):
+						next_idx = randi_range(0, len(transforms) - 1)
+						if (next_idx != serial_last_index_preview) and (next_idx != serial_index_preview):
+							break
+					serial_last_index_preview = serial_index_preview
+					serial_index_preview = next_idx
+				print(serial_last_index_preview, " -> ", serial_index_preview)
+			ButtonEditorButtons.TransformObject:
+				if mode == ButtonMode.TransformSerial or mode == ButtonMode.VariableSerial:
+					serial_or_random = true
+					serial_index += 1
+					if serial_index >= len(transforms):
+						serial_index = 0
+					serial_last_index = serial_index - 1
+					if serial_last_index < 0:
+						serial_last_index = len(transforms) - 1
+				elif mode == ButtonMode.TransformRandom or mode == ButtonMode.VariableRandom:
+					serial_or_random = true
+					var next_idx = -1
+					for i in range(0, 100):
+						next_idx = randi_range(0, len(transforms) - 1)
+						if (next_idx != serial_last_index) and (next_idx != serial_index):
+							break
+					serial_last_index = serial_index
+					serial_index = next_idx
+				print(serial_last_index, " -> ", serial_index)
+		if serial_or_random:
+			var last_transform: Transform3D
+			var next_transform: Transform3D
+			match which_button:
+				ButtonEditorButtons.PreviewTransformGizmo, ButtonEditorButtons.PreviewTransformObject:
+					last_transform = transforms[serial_last_index_preview]
+					next_transform = transforms[serial_index_preview]
+				ButtonEditorButtons.TransformObject:
+					last_transform = transforms[serial_last_index]
+					next_transform = transforms[serial_index]
+			if was_running:
+				last_transform = current_transform
+			last_quat = Quaternion(last_transform.basis.orthonormalized())
+			next_quat = Quaternion(next_transform.basis.orthonormalized())
+			last_pos = last_transform.origin
+			next_pos = next_transform.origin
+			last_scale = last_transform.basis.get_scale()
+			next_scale = next_transform.basis.get_scale()
+@export var blendtime: float = 2
+@export var stepped: bool = false
+@export var steps: int = 5
 
 var debug_color: Color = Color.MAGENTA
 
@@ -86,7 +214,7 @@ func make_transform(append: bool = true):
 		notify_property_list_changed()
 
 func _ready() -> void:
-	debug_color = Color.from_ok_hsl(randf(), randf_range(0.8, 1.0), randf_range(0.2, 0.8))
+	debug_color = Color.from_ok_hsl(randf(), randf_range(0.8, 1.0), randf_range(0.2, 0.9))
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
@@ -99,3 +227,46 @@ func _process(delta: float) -> void:
 					xf_draw = xf_draw.translated(self.global_position)
 				DebugDraw3D.draw_gizmo(xf_draw, DebugDraw3D.empty_color, true)
 				DebugDraw3D.draw_line(self.global_position, xf_draw.origin, debug_color)
+
+func stepped_01(x: float, steps: int) -> float:
+	return min(1, floor(float(steps) * x) / (steps - 1))
+
+## returns true when finished
+func transform_action(last_quat: Quaternion, next_quat: Quaternion, last_pos: Vector3, next_pos: Vector3, time01: float, apply: bool = true) -> bool:
+	var blend_factor = time01
+	if stepped:
+		blend_factor = stepped_01(time01, steps)
+	var blended_quat = last_quat.slerp(next_quat, blend_factor)
+	var blended_pos = last_pos.lerp(next_pos, blend_factor)
+	var blended_scale = last_scale.lerp(next_scale, blend_factor)
+	current_transform = Transform3D(Basis(blended_quat).scaled(blended_scale), blended_pos)
+	var use_transform = current_transform
+	if transform_relative_position:
+		use_transform = current_transform.translated(self.global_position)
+	if apply:
+		get_node(nodepath).global_transform = use_transform
+	else:
+		DebugDraw3D.draw_gizmo(use_transform, DebugDraw3D.empty_color, true)
+		DebugDraw3D.draw_line(self.global_position, use_transform.origin, debug_color)
+	return false
+
+func transform_action_finished():
+	pass
+
+func _physics_process(delta: float) -> void:
+	if is_running:
+		#print(accumulator)
+		#ButtonMode.TransformBlendpath:
+			#last_transform = transforms[blend_last_index]
+			#next_transform = transforms[blend_index]
+		if (preview and Engine.is_editor_hint()) or not preview:
+			transform_action(last_quat, next_quat, last_pos, next_pos, accumulator, apply_if_valid)
+		if run_count >= inc_count:
+			# done
+			print("finished")
+			run_count = 0
+			is_running = false
+			if reset_transform:
+				get_node(nodepath).global_transform = original_transform
+		accumulator += increment
+		run_count += 1
